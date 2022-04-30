dbd59a696325cd511f3d48cce8fe53d3f5289d0f
Mem U Sins <83538724+memUsins@users.noreply.github.com>
2022-04-30 18:13:38 +0300

    Add header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,54 @@
 // js
+
+let sidebarOpenButton = document.querySelector(".header__sidebar-bar");
+let sidebarCloseButton = document.querySelector(".sidebar__bar");
+let sidebar = document.querySelector(".sidebar");
+let overlay = document.querySelector(".overlay");
+
+const activeClassName = "active";
+const modalList = [sidebar, overlay];
+
+const showItem = (data, className = activeClassName) => {
+    if (Array.isArray(data)) {
+        data.forEach((item) => {
+            item.classList.add(className);
+        })
+    } else {
+        data.classList.add(className);
+    }
+}
+const hideItem = (data, className = activeClassName) => {
+    if (Array.isArray(data)) {
+        data.forEach((item) => {
+            item.classList.remove(className);
+        })
+    } else if (data === "all") {
+        modalList.forEach((item) => {
+            if (item.classList.contains(className)) {
+                item.classList.remove(className);
+            }
+        });
+    } else {
+        data.classList.remove(className);
+    }
+}
+
+const accordeonFunc = (item, className) => {
+    if (!item.classList.contains(className)) {
+        showItem(item);
+    } else {
+        hideItem(item);
+    }
+}
+
+sidebarOpenButton.addEventListener("click", () => {
+    showItem([sidebar, overlay]);
+});
+
+sidebarCloseButton.addEventListener("click", () => {
+    hideItem([sidebar, overlay]);
+});
+
+overlay.addEventListener("click", () => {
+    hideItem("all");
+});
