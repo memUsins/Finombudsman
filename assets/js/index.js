fbd50714630ea3ae872e9fc0326586132abc44a8
Mem U Sins <83538724+memUsins@users.noreply.github.com>
2022-05-06 21:56:51 +0300

    push

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,6 +14,7 @@ let search = document.querySelector(".header__search-mobile .hide");
 let overlay = document.querySelector(".overlay");
 let body = document.querySelector("body");
 let modalClose = document.querySelector(".modal__close");
+let modalCloseBtn = document.querySelector(".button_close");
 
 const activeClass = "active";
 const uiList = [body, overlay, sidebar, modalClose];
@@ -57,6 +58,7 @@ modal(modalsAll);
 
 overlay.addEventListener("click", () => hideItem("all"));
 modalClose.addEventListener("click", () => hideItem("all"));
+if (modalCloseBtn) modalCloseBtn.addEventListener("click", () => hideItem("all"));
 
 // Tabs
 const tab = (data) => {
@@ -177,3 +179,81 @@ contentSidebar.forEach((item) => {
 // Sidebar
 searchOpen.addEventListener("click", () => showItem(search));
 searchClose.addEventListener("click", () => hideItem(search));
+
+// quiz
+
+let quizNextCtrl = document.querySelectorAll(".quiz_next-ctrl")
+let quizPrevCtrl = document.querySelectorAll(".quiz_prev-ctrl")
+
+let quizNextScreen = (item) => {
+    let currentItem = item.parentNode.parentNode;
+    let nextItem = currentItem.nextElementSibling;
+
+    if (nextItem != null) {
+        hideItem(currentItem);
+        showItem(nextItem);
+    }
+}
+
+quizPrevCtrl.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault()
+        let currentItem = item.parentNode.parentNode;
+        let prevItem = currentItem.previousElementSibling;
+
+        if (prevItem != null) {
+            hideItem(currentItem);
+            showItem(prevItem);
+        }
+    });
+});
+
+let firstQuizCtrl = document.querySelectorAll(".quiz_first-screen__next");
+
+firstQuizCtrl.forEach(item => {
+    item.addEventListener("click", (e) => {
+        e.preventDefault()
+        quizNextScreen(item);
+    });
+});
+
+let secondQuizCtrl = document.querySelector(".quiz_second-screen__next");
+let warningModal = document.querySelector('#warning-modal-content');
+uiList.push(warningModal);
+
+secondQuizCtrl.addEventListener("click", (e) => {
+    e.preventDefault()
+    let inputs = document.querySelectorAll('.input-file');
+
+    let errorList = [];
+
+    inputs.forEach((field) => {
+        console.log(field.files.length)
+        if (field.files.length == 0) {
+            showItem([warningModal, overlay, body])
+            errorList.push(true)
+        }
+    })
+    if (errorList.length == 0) quizNextScreen(secondQuizCtrl);
+});
+
+let thirdQuizCtrl = document.querySelector(".quiz_third-screen__next");
+
+thirdQuizCtrl.addEventListener("click", (e) => {
+    e.preventDefault()
+    quizNextScreen(thirdQuizCtrl);
+});
+
+// let formFiles = document.querySelector("#form-files");
+
+let inputs = document.querySelectorAll('.input-file');
+
+inputs.forEach((item) => {
+    item.addEventListener('change', () => {
+        if (item.files.length > 0) {
+            let fileInfo = item.previousElementSibling;
+            fileInfo.classList.add('active')
+            fileInfo.querySelector('.title').innerHTML = item.files[0].name;
+        }
+    })
+})
